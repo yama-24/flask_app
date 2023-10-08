7bea0e9806d33a49132b087271b1b4d371b46cb8
yama-24 <yamamoto.masanori.se@gmail.com>
2023-10-08 16:31:14 +0900

    2st commmit

--- src/view.py
+++ src/view.py
@@ -11,8 +11,12 @@ app = Flask(__name__)
 
 # def以下がアクセス後の操作
 def index():
+    # DBから以下の変数を読み込んできたと仮定
+    title_ = 'ようこそ'
+    message_ = 'MTVデザインパターンでWebアプリ作成'
+
     # return 'Hello World!'
-    return render_template('index.html') # 追加
+    return render_template('index.html', title=title_, message=message_)
 
 if __name__ == '__main__':
     app.run()
